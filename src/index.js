27853e2607e9efd3a1721a0589902a7637e36305
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-08-30 17:20:08 -0600

    Add function to fetch  weather API data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+async function getForecastWeatherData(location) {
+  try {
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=e990e820b2c64eef80a155450232508&q=${location}&days=3`;
+    const options = {
+      mode: "cors",
+    };
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error("Failed to connect to API");
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getForecastWeatherData("mexico_city");
